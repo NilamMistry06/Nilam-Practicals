fa502c3c3166ddf1f2292389072a0e8af290b182
nilam mistry <nilammistry0694@gmail.com>
2024-09-05 08:57:45 +1000

    Unit Testing Demo

---
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,19 @@
+﻿namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int number)
+        {
+            if(number < 2) return false;
+
+            //throw new NotImplementedException("Not implemented.");
+            if (number == 2) return true;
+
+            for(int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
